154c986115dd66e8e1baa1daae06f6aba922b9d5
saintdianabless <93075932+saintdianabless@users.noreply.github.com>
2023-07-16 08:10:22 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Damo;
+
+var damo = DmSoft.LoadAndUnlock();
+Console.WriteLine(damo.Ver());
+var handle = damo.FindWindow("扫雷", "扫雷");
+var bind = damo.BindWindowEx(handle, "normal", "windows", "normal", "", 0);
+if (bind != 1)
+{
+    Console.WriteLine("failed to bind window");
+    Environment.Exit(1);
+}
+
+var mines = damo.ReadInt(handle, "01005330", 0);
+var width = damo.ReadInt(handle, "01005334", 0);
+var height = damo.ReadInt(handle, "01005338", 0);
+Console.WriteLine($"{mines} {width} {height}");
+var Sweep = (int x, int y) =>
+{
+    var tx = 1 + 16 * x;
+    var ty = 50 + 16 * y;
+    // var pos = damo.ClientToScreen(handle, tx, ty);
+    // damo.MoveTo(pos.X, pos.Y);
+    damo.MoveTo(tx, ty);
+    damo.LeftClick();
+};
+
+Thread.Sleep(3000);
+
+var boardBase = 0x1005340;
+for (int y = 1; y <= height; y++)
+{
+    for (int x = 1; x <= width; x++)
+    {
+        var v = damo.ReadData(handle, (boardBase + x + 32 * y).ToString("X4"), 1);
+        if (v == "0F")
+        {
+            Sweep(x, y);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# 大漠6.1544 X C#
+
+### 仅供学习交流,严禁用于其它任何用途
+### dll文件virustotal报毒, 请自行评估
+
+dotnet run -r win-x86
